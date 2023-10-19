5d660898aeae10d2d9a136d3af205df240b639e6
Umut Erozan <umut_erozan@hotmail.com>
2023-10-19 17:05:22 +0300

    vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-random-byte-generator/",
   plugins: [react()],
   resolve: {
     alias: {
